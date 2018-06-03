baa685067499db20c97d06aca8d07772f162451b
Wataru Kume <kumewata@gmail.com>
2018-06-03 12:59:31 +0000

    refactor varable names

--- app/controllers/energy_productions_controller.rb
+++ app/controllers/energy_productions_controller.rb
@@ -24,36 +24,17 @@ class EnergyProductionsController < ApplicationController
     
     # prepare city list for grouping
     city_list = House.distinct.pluck(:city)
-    # @check = energy_production.group("label").average("energy_production")
-    # debugger
     
     # prepare data for line chart
-    # @data_line = {}
-    gon.data_line2 = {}
+    gon.data_line = {}
     city_list.each do |city|
       
       eph_h = energy_production_house.where(houses: { city: city })
       
-      
-      # production = []
-      # label_list.each do |label|
-        
-      #   eph_h_l = eph_h.where(label: label)
-      #   ep_list = []
-      #   eph_h_l.each{ |e| ep_list << e.energy_production }
-        
-      #   # calculate average of energy production
-      #   ep_ave = ave_calc(ep_list) 
-      #   production << ep_ave
-      # end
-      # #pass data
-      # @data_line[city] = production
-      
-      # パフォーマンス的に上よりこっちの方が良いが、Heroku環境でうまく行かず途中
+      # pass data
       production = eph_h.group("label").average("energy_production")
-      gon.data_line2[city] = production.values.map{ |v| v.round(1)}
+      gon.data_line[city] = production.values.map{ |v| v.round(1)}
     end
-    # debugger
     
   end
   
