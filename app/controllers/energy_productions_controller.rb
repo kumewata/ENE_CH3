986ebd5b5a442c2a491fd1c0d15d172d59af9f68
Wataru Kume <kumewata@gmail.com>
2018-06-03 06:08:26 +0000

    add gom gem for passing array_data to JS, and add bar graph

--- app/controllers/energy_productions_controller.rb
+++ app/controllers/energy_productions_controller.rb
@@ -1,4 +1,5 @@
 class EnergyProductionsController < ApplicationController
+  include CommonCalc
   
   # def make_list(model, column)
   #   columns = model.select(:column).distinct
@@ -10,7 +11,27 @@ class EnergyProductionsController < ApplicationController
   #   return column_list
   # end
   
+  def self.label_month_converter(model, labels)
+    year = nil
+    label_map = []
+    
+    labels.each do |label|
+      r = model.find_by(label: label)
+      
+      if year == nil || r.year > year
+        year = r.year
+        label_map << "#{r.year}/#{r.month}"
+        next
+      end
+      
+      label_map << "#{r.month}"
+    end
+    
+    return label_map
+  end
+  
   def index
+    
     # @energy_production = EnergyProduction.all.includes(:house)
     @energy_production = EnergyProduction.includes(:house)
     @house = House.all
@@ -23,11 +44,12 @@ class EnergyProductionsController < ApplicationController
     labels.each do |l|
       label_list << l.label
     end
-    # label_list_t = ['7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6']
-    label_list_t = ["7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6"]
+    
+    gon.label_list_t = self.class.label_month_converter(@e_p, label_list)
+    # debugger
     
     @energy_production_hash = {}
-    @energy_production_hash['label'] = label_list_t
+    # @energy_production_hash['label'] = gon.label_list_t
     @energy_production_hash['data'] = {}
     
     # グルーピング用にcityの配列を用意
@@ -36,6 +58,13 @@ class EnergyProductionsController < ApplicationController
     cities.each do |c|
       city_list << c.city
     end
+    count_result = @house.group(:city).count
+    # 棒グラフ用の(x, y)データ
+    gon.city_list_t = count_result.keys # 横軸: city
+    gon.house_count_t = count_result.values # 横軸: city
+    
+    
+    # debugger
     
     city_list.each do |city|
       
@@ -50,14 +79,15 @@ class EnergyProductionsController < ApplicationController
         epl.each do |e|
           ep_list << e.energy_production
         end
-        #  epの平均値を得る
-        ep_ave =  ep_list.inject(0.0){|r,i| r+=i }/ep_list.size
+        # epの平均値を得る
+        ep_ave = ave_calc(ep_list) 
         production << ep_ave
       end
       
       @energy_production_hash['data'][city] = production
       
     end
+    gon.energy_production_list_t = @energy_production_hash['data']
     # debugger
   end
 end
